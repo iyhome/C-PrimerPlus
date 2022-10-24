848b9fecae43ac031b570f71a5b30c4aaad38d5c
iyhome <iyhome@icloud.com>
2022-10-24 21:39:40 +0800

    homework for 3

--- homeWork3/3_1.cpp
+++ homeWork3/3_1.cpp
@@ -1,7 +1,20 @@
-//
+/* 编写一个小程序，要求用户使用一个整数指出自己的身高（单位为英寸），
+然后将身高转换为英尺和英寸。该程序使用下划线字符来指示输入位置。另外，使用一个 const 符号常量来表示转换因子。 */
+
 #include <iostream>
+using namespace std;
 
-int main()
-{
+double feet2inch(double feetHeight){
+    const double transForm = 0.083;
+    return (transForm * feetHeight);
+}
 
+int main(){
+    double heightInch, heightFeet;
+    cout << "input your height:___\b\b\b";
+    cin >> heightFeet;
+    heightInch = feet2inch(heightFeet);
+    cout << endl
+        << "your height using feet is  " << heightFeet << endl
+        << "your height using inch is  " << heightInch << endl;
 }
---
+++ homeWork3/3_5.cpp
@@ -0,0 +1,8 @@
+/*
+    编写一个程序，要求用户输入全球当前的人口和美国当前的人口（或其他国家的人口）。
+将这些信息存储在 long long 变量中，并让程序显示美国（或其他国家）
+的人口占全球人口的百分比。该程序的输出应与下面类似：
+    Enter the world's population: 6898758899
+    Enter the population of the US: 310783781
+    The population of the US is 4.50492% of the world population.
+*/
